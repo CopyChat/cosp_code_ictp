671141f0d370c0b083779a874b38d17d62bec677
SMR2831 Group 7 <grp07@argo-login2.ictp.it>
2016-05-27 23:44:01 +0200

    add some words, more readable

--- cosp_driver.sh
+++ cosp_driver.sh
@@ -3,12 +3,13 @@
 #
 #          FILE: cosp_driver.sh
 # 
-USAGE="./cosp_driver.sh [ -t ] + infile "
+USAGE="./cosp_driver.sh [ -t ] "
 # 
 #   DESCRIPTION:  to drive cosp_test
 # 
 #       OPTIONS: --- t for test
-#  REQUIREMENTS: --- cosp_cmor_nl.txt, cosp_input_nl.txt,cosp_output_nl.txt
+#  REQUIREMENTS: --- cosp_cmor_nl.temp.txt, cosp_input_nl.temp.txt, cosp_output_nl.txt
+#                    awk, cdo
 #          BUGS: ---
 #         NOTES: ---
 #        AUTHOR: Tang (Tang), chao.tang.1@gmail.com
@@ -29,36 +30,47 @@ done
 shift $(($OPTIND - 1))
 #=================================================== 
 
+startday="2011-01-01"  # default start day in cosp_cmor_nl.txt
+infile_prefix="COSP_SUBEX"  # input file, e.g. COSP_SUBEX_2011-01-98.nc
+
 
 for j in 2011 # Year
 do
-    for i in  01  # month
+    for i in  01 07  # month, if you add other months, please take care of the limits of timestep, in next loop.
     do
-        for ((timestep = 0; timestep < 124 ; timestep++))
+        for ((timestep = 0; timestep < 124 ; timestep++))  
         do
+            # Josefina's formula to convert timesteps to day
             day=$(echo $timestep | awk '{print (($1)-($1)%4)/4 +1}' | awk '{if( $1 < 10)print 0$1;else print $1}')
             echo ================== day = $day
+
+            # make a directory for each timestep
             mkdir -p cloud_"$j"-"$i"-"$timestep"
             #FINPUT='COSP_1.nc'
 #=================================================== change namelist file
-            # change the starttime
+
+            #NOTE: if you want simulator to output the same named file, comment/delete this line, maybe convinient for post-processing
+            startday=$(echo "'$j'-'$i'-'$day'")
+
+            # change the starttime and output dir; 
             cat ./cosp_cmor_nl.temp.txt | awk '{\
-                gsub(/date90908/,"'$j'-'$i'-'$day'");\
                 gsub(/output90908/,"cloud_'$j'-'$i'-'$timestep'");\
+                gsub(/date90908/,"'$startday'");\  
                 print}' > \
                 ./cosp_cmor_nl.txt
 
             # change the input data
             cat ./cosp_input_nl.temp.txt | awk '{\
-                gsub(/input90908/,"COSP_SUBEX_'$j'-'$i'-'$timestep'.nc");\
+                gsub(/input90908/,"'$infile_prefix'_'$j'-'$i'-'$timestep'.nc");\
                 print}' > \
                 ./cosp_input_nl.txt
 #=================================================== end of change namelist file
+
+            # call the exe of simulator
             ./cosp_test > cosp.logout
 
         done
     done
 done
-
 
 
